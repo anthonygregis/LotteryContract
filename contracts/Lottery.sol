c0c261f949124e1d57cecfd937b14d193d47985e
Anthony Gregis <anthony@thedevtron.com>
2021-04-26 08:45:54 -0500

    [Lottery] Adds ability to enter lottery, requires minimum ether amount

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,8 +3,25 @@ pragma solidity ^0.8.4;
 
 contract Lottery {
   address public manager;
+  address[] public players;
+
+  modifier minEther() {
+    require(msg.value > .01 ether, "Send more then .01 ether");
+    _;
+  }
+
+  event playerEntered(address indexed _from, uint _value);
 
   constructor () {
     manager = msg.sender;
+  }
+
+  function getPlayers() public view returns (address[] memory) {
+    return players;
+  }
+
+  function enter() public payable minEther {
+    players.push(msg.sender);
+    emit playerEntered(msg.sender, msg.value);
   }
 }
