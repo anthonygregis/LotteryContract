4339f399db39afa3aa179db5e80f8681388b2872
Anthony Gregis <anthony@thedevtron.com>
2021-04-26 10:33:09 -0500

    [Testing] Adds full test suite for Lottery

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -63,4 +63,54 @@ describe("Lottery Contract", () => {
     assert.strictEqual(accounts[3], players[2]);
     assert.strictEqual(3, players.length);
   });
+
+  it("requires minimum amount of ether to enter", async () => {
+    let executed;
+
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[1],
+        value: 0,
+      });
+      executed = "Account Added";
+    } catch (err) {
+      executed = "Account Failed";
+    }
+
+    assert.strictEqual("Account Failed", executed);
+  });
+
+  it("Only manager can pick winner", async () => {
+    let executed;
+
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[1],
+      });
+      executed = true;
+    } catch (err) {
+      executed = false;
+    }
+
+    assert.strictEqual(false, executed);
+  });
+
+  it("runs a full lottery", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei("2", "ether"),
+    });
+
+    const enterBalance = await web3.eth.getBalance(accounts[1]);
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+    const winningBalance = await web3.eth.getBalance(accounts[1]);
+    const balanceDifference = winningBalance - enterBalance;
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[1],
+    });
+
+    assert(balanceDifference > web3.utils.toWei("1.8", "ether"));
+    assert.strictEqual(0, players.length);
+    assert.strictEqual("0", await web3.eth.getBalance(lottery.options.address));
+  });
 });
